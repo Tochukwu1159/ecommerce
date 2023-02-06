1b44d82ed9280c2a6b1f75db75b7f445529a0d3f
Tochukwu1159 <christinotochukwu@gmail.com>
2023-02-06 20:44:26 +0100

    setup

--- src/main/java/com/code/ecommerce/controller/ProductCommandController.java
+++ src/main/java/com/code/ecommerce/controller/ProductCommandController.java
@@ -1,4 +1,10 @@
 package com.code.ecommerce.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/prod")
 public class ProductCommandController {
+
 }
